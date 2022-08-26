fc8ec6b51a2980600d395dcc8bd5489b89501e4f
Jim Kadin <james.kadin@jax.org>
2022-08-26 15:43:35 -0400

    output format tweaks. show all diffs w/o a predtype option.

--- diffRoutings.sh
+++ diffRoutings.sh
@@ -3,38 +3,53 @@
 function Usage() {
     cat - <<ENDTEXT
 
-$0 TP|FP|TN|FN RoutingFile1 RoutingFile2
+$0 [-s] [-TP|-FP|-TN|-FN] RoutingFile1 RoutingFile2
 
-Compare two routing files to report a specific predType (TP, FP, TN, FN)
-    in one file and not the other
+Diff two routing files to report routing differences. Default: all differences.
+   -s   short diff output:  < lines in the 1st file, > lines in the 2nd
+   -TP  only report true positives in one file and not the other
+   -FP  etc.
+   -TN  etc.
+   -FN  etc.
 
-Example:   $0 FN R1 R2
-                reports FN's that are in R1 and not in R2
+Example:   $0 -FN R1 R2
+           reports FN's that are in R1 and not in R2, and vice versa
 ENDTEXT
     exit 5
 }
 
+PREDTYPE=""     # no specific predtype
+SHORTOUTPUT='0'
 while [ $# -gt 0 ]; do
     case "$1" in
     -h|--help) Usage ;;
+    -s)       SHORTOUTPUT=1; shift;;
+    -TP|--TP) PREDTYPE=TP; shift ;;
+    -TN|--TN) PREDTYPE=TN; shift ;;
+    -FP|--FP) PREDTYPE=FP; shift ;;
+    -FN|--FN) PREDTYPE=FN; shift ;;
     -*|--*) echo "invalid option $1"; Usage ;;
     *) break; ;;
     esac
 done
-if [ $# -ne 3 ]; then
+if [ $# -ne 2 ]; then
     Usage
 fi
 
-PREDTYPE="$1"
-R1="$2"
-R2="$3"
-R1_short="tmp.$$.r1.short"
-R2_short="tmp.$$.r2.short"
+R1="$1"
+R2="$2"
+R1_nocounts="tmp.$$.r1.nocounts"
+R2_nocounts="tmp.$$.r2.nocounts"
 
 # get files with truncated routings, so the counts don't cause diffs
 # -f 1-4:    ID, knownClassName, routing, predType
-cut -d "|" -f 1-4 $R1 | grep $PREDTYPE> $R1_short
-cut -d "|" -f 1-4 $R2 | grep $PREDTYPE> $R2_short
-diff $R1_short $R2_short | egrep "[<>]"
+cut -d "|" -f 1-4 $R1 | grep "$PREDTYPE" > $R1_nocounts
+cut -d "|" -f 1-4 $R2 | grep "$PREDTYPE" > $R2_nocounts
 
-rm -f $R1_short $R2_short
+if [ "$SHORTOUTPUT" -eq "1" ]; then
+    diff $R1_nocounts $R2_nocounts | egrep "[<>]"
+else
+    diff $R1_nocounts $R2_nocounts
+fi
+
+rm -f $R1_nocounts $R2_nocounts
